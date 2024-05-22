748690cecd989ef1e4bc10487f628d8437cfd38f
Ashish <theashish752@gmail.com>
2024-05-22 19:42:22 +0530

    add image configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-  
 export default nextConfig;
